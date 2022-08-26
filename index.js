db702f7666838469fa68448bfe047f6976f4176f
Rusanganwa Emmanuel <rusanganwa52@gmail.com>
2022-08-26 13:06:58 +0200

    Feat:fouth commit Add all {Finishs#}# {Finishs#}

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const images=[
     },
 
     {
-        Name:"orange",
-        image:'./orange.jpg',
+        Name:"oranges",
+        image:'./oranges.jpg',
 
     },
     
@@ -41,12 +41,59 @@ const images=[
         image:'./stilo.jpg',
 
     },
+    {
+        Name:"mandalene",
+        image:'./mandalene.jpg',
+
+    },
     
 ];
 
-const photo=document.getElementById('photo');
-const Name=document.getElementById('Name')
-{images.map((image)=>{
-        photo.innerHTML += `<img src='${image.image}'>`
-    })
+// const photo=document.getElementById('photo');
+// const Name=document.getElementById('Name')
+// {images.map((image)=>{
+//         photo.innerHTML += `<img src=${image.image}>`
+//         photo.innerHTML +=`${image.Name}
+
+//         `
+//     })
+    
+
+function image(image,Name){
+
+const photos=`
+
+<div class="card">
+  <div class="card-body"><img src=${image} </div>
+  <div class="card-footer">${Name}</div>
+</div>
+`
+  photo.innerHTML+=photos;  
 }
+
+
+images.map(data=>{
+    image(data.image,data.Name);
+})
+ const data=document.getElementById("seach");
+
+ const search=document.getElementById("cocktail");
+
+ const grass=(value)=>{
+    photo.innerHTML=" ";
+const items=images.filter(datas=>datas.Name.toLowerCase().includes(value.toLowerCase()))
+items.map(item=>image(item.image,item.Name))
+ }
+
+
+ data.addEventListener("keyup",e=>{
+    grass(data.value)
+ })
+
+ search.addEventListener("keyup",e=>{
+    console.log(e.keyCode);
+    if(e.keyCode==13){
+        grass(search.value)
+    }
+ })
+
